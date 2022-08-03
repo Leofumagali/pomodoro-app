e57aeb7816b86aef6822c8e429015d6cec7ffcf0
Leo Fumagali <leonardo.fumagali@gmail.com>
2022-08-03 13:33:02 -0300

    Router bug fixed

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
-import { DefaultLayout } from './layouts/DefaultLayout/index'
-import { History } from './pages/History/index'
-import { Home } from './pages/Home/index'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { History } from './pages/History'
+import { Home } from './pages/Home'
 
 export function Router() {
   return (
